15b4b84fd391cb75027fe66f27bdd17622156873
Naseer Ahmed <naseer@codeaurora.org>
2013-06-25 17:58:16 -0400

    msm: display: command mode panel release fence
    For command mode panel, release fence should be signaled after the buffer is
    sent to the panel.  For performance consideration, allow one more commit to be
    queued.  Keep the second kick off as short as enough.
    
    Change-Id: I19af075ebf480d3e81b46b1cd2ef21a2893f143e
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/mdp4_overlay_dsi_cmd.c

--- drivers/video/msm/mdp4_overlay_dsi_cmd.c
+++ drivers/video/msm/mdp4_overlay_dsi_cmd.c
@@ -252,7 +252,7 @@ void mdp4_dsi_cmd_pipe_queue(int cndx, struct mdp4_overlay_pipe *pipe)
 
 static void mdp4_dsi_cmd_blt_ov_update(struct mdp4_overlay_pipe *pipe);
 
-int mdp4_dsi_cmd_pipe_commit(int cndx, int wait)
+int mdp4_dsi_cmd_pipe_commit(int cndx, int wait, u32 *release_busy)
 {
 	int  i, undx;
 	int mixer = 0;
@@ -380,9 +380,21 @@ int mdp4_dsi_cmd_pipe_commit(int cndx, int wait)
 	mdp4_stat.overlay_commit[pipe->mixer_num]++;
 
 	if (wait) {
+<<<<<<< HEAD
 		long long tick;
 
 		mdp4_dsi_cmd_wait4vsync(0, &tick);
+=======
+		if (release_busy) {
+			msm_fb_release_busy(vctrl->mfd);
+			*release_busy = false;
+			mutex_unlock(&vctrl->mfd->dma->ov_mutex);
+		}
+		if (pipe->ov_blt_addr)
+			mdp4_dsi_cmd_wait4ov(0);
+		else
+			mdp4_dsi_cmd_wait4dmap(0);
+>>>>>>> 18db125... msm: display: command mode panel release fence
 	}
 
 	return cnt;
@@ -1166,9 +1178,14 @@ void mdp4_dsi_cmd_overlay(struct msm_fb_data_type *mfd)
 	}
 
 	mdp4_overlay_mdp_perf_upd(mfd, 1);
+<<<<<<< HEAD
 
 	mutex_lock(&mfd->dma->ov_mutex);
 	mdp4_dsi_cmd_pipe_commit(0, 0);
+=======
+	mdp4_dsi_cmd_pipe_commit(cndx, 1, NULL);
+	mdp4_overlay_mdp_perf_upd(mfd, 0);
+>>>>>>> 18db125... msm: display: command mode panel release fence
 	mutex_unlock(&mfd->dma->ov_mutex);
 
 	mdp4_overlay_mdp_perf_upd(mfd, 0);
